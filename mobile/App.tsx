4f0cf128cc8f9f29f0d6ccc05041b864f6e26eb5
Marina Oliveira <marina09oliveira@gmail.com>
2020-10-15 16:12:27 -0300

    aula 4 inicio do mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    // retorna true or false baseado se as fontes estão carregadas ou n
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Routes />;
+}
